a9dd47997b294f5bd890e9e27422de3fb3a98815
Mohammadreza Sharifi <52376714+rezasharifi82@users.noreply.github.com>
2022-10-24 16:00:53 +0330

    Update Q4.sparse_product.py

--- hw1/Q4.sparse_product.py
+++ hw1/Q4.sparse_product.py
@@ -5,17 +5,16 @@ def sparse_product(a:list , b:list):
     if(a0[1]!=b0[0]):
         print("Invalid Case!")
     else:
+        # c=a0[0]*b0[1]
+        # conj=++
         c=[[a0[0],b0[1],0]]
         for i in range(1,len(a)):
             for j in range(1,len(b)):
                 if(a[i][0]==b[j][0] and a[i][1]==b[j][1]):
-                    c.append([a[i][0],a[i][1],a[i][-1]*b[j][-1]])
+                    c.append([i-1,j-1,a[i][-1]*b[j][-1]])
                     c[0][-1]+=1
 
         print(*c,sep="\n")
-        
-# Why this algorithm works?
-# because of some useful properties in the sparse matrix
 
 a=[ [4,5,4],
     [0,2,3],
@@ -28,6 +27,7 @@ b=[ [5,3,3],
     [0,1,5],
     [3,1,4]
 ]
+# print(a[-1])
 sparse_product(a,b)
 
     
